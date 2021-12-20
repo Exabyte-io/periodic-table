e629d478be928c318cde2627a25a2d28df965179
tjduigna <thomas.j.duignan@gmail.com>
2021-12-20 10:39:19 -0500

    feat: applies style to src and test files

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
 import _ from "underscore";
 
-import {ELEMENT_BONDS} from "./element_bonds";
-import {PERIODIC_TABLE} from "./periodic_table";
-import {ELEMENT_COLORS} from "./element_colors";
-import {ELEMENT_VDW_RADII} from "./element_vdwRadii";
+import { ELEMENT_BONDS } from "./element_bonds";
+import { ELEMENT_COLORS } from "./element_colors";
+import { ELEMENT_VDW_RADII } from "./element_vdwRadii";
+import { PERIODIC_TABLE } from "./periodic_table";
 
-export {ELEMENT_BONDS};
-export {PERIODIC_TABLE};
-export {ELEMENT_COLORS};
-export {ELEMENT_VDW_RADII};
+export { ELEMENT_BONDS };
+export { PERIODIC_TABLE };
+export { ELEMENT_COLORS };
+export { ELEMENT_VDW_RADII };
 
-export const ELEMENTS_BY_SYMBOL = _.reduce(PERIODIC_TABLE, function (memo, item) {
-    memo[item.symbol] = item;
-    return memo;
-}, {});
+export const ELEMENTS_BY_SYMBOL = _.reduce(
+    PERIODIC_TABLE,
+    (memo, item) => {
+        memo[item.symbol] = item;
+        return memo;
+    },
+    {},
+);
 
 /**
  * Returns element electronegativity by symbol.
@@ -22,7 +26,7 @@ export const ELEMENTS_BY_SYMBOL = _.reduce(PERIODIC_TABLE, function (memo, item)
  */
 export function getElectronegativity(symbol) {
     const config = ELEMENTS_BY_SYMBOL[symbol];
-    return config ? config["pauling_negativity"] : 0;  // return zero if value cannot be accessed by symbol
+    return config ? config.pauling_negativity : 0; // return zero if value cannot be accessed by symbol
 }
 
 /**
@@ -33,10 +37,17 @@ export function getElectronegativity(symbol) {
  * @param order {Number} the bond number
  * @returns {Array}
  */
-export function filterBondsDataByElementsAndOrder(bondsData, element1, element2, order = undefined) {
-    return bondsData.filter(b => {
-        return b.elements.sort().toString() === [element1, element2].sort().toString()
-            && (order ? b.order === order : true);
+export function filterBondsDataByElementsAndOrder(
+    bondsData,
+    element1,
+    element2,
+    order = undefined,
+) {
+    return bondsData.filter((b) => {
+        return (
+            b.elements.sort().toString() === [element1, element2].sort().toString() &&
+            (order ? b.order === order : true)
+        );
     });
 }
 
@@ -51,20 +62,17 @@ export function defaultElementsBondsDataEntry(element1, element2, order = undefi
     const element1CovalentRadius = ELEMENTS_BY_SYMBOL[element1].covalent_radius_pm / 100;
     const element2CovalentRadius = ELEMENTS_BY_SYMBOL[element2].covalent_radius_pm / 100;
     return {
-        "elements": [
-            element1,
-            element2
-        ],
-        "energy": {
-            "value": "",
-            "units": "eV"
+        elements: [element1, element2],
+        energy: {
+            value: "",
+            units: "eV",
         },
-        "length": {
-            "value": element1CovalentRadius + element2CovalentRadius,
-            "units": "angstrom"
+        length: {
+            value: element1CovalentRadius + element2CovalentRadius,
+            units: "angstrom",
         },
-        "order": order
-    }
+        order,
+    };
 }
 
 /**
@@ -93,7 +101,9 @@ export function getElementsBondsData(element1, element2, order = undefined) {
  * @returns {Number}
  */
 export function getElementAtomicRadius(elementSymbol) {
-    const elementName = Object.keys(PERIODIC_TABLE).find(key => elementSymbol === PERIODIC_TABLE[key].symbol);
+    const elementName = Object.keys(PERIODIC_TABLE).find(
+        (key) => elementSymbol === PERIODIC_TABLE[key].symbol,
+    );
     if (elementName) {
         return PERIODIC_TABLE[elementName].atomic_radius_pm * 0.01;
     }
