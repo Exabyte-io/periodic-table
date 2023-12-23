8d01946ba9cb156e37df39c9efa712055a40ef51
Timur Bazhirov <timur@exabyte.io>
2023-12-22 22:57:26 -0800

    chore: fix py test in CI

--- src/py/periodic_table/__init__.py
+++ src/py/periodic_table/__init__.py
@@ -1,12 +1,8 @@
-import os
 import json
-from pathlib import Path
 
-CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
-
-f1 = open(f"{CURRENT_DIR.parent.parent.parent}/element-bonds.json")
+f1 = open("./element-bonds.json")
 ELEMENT_BONDS = json.load(f1)
-f2 = open(f"{CURRENT_DIR.parent.parent.parent}/element-colors.json")
+f2 = open("./element-colors.json")
 ELEMENT_COLORS = json.load(f2)
-f3 = open(f"{CURRENT_DIR.parent.parent.parent}/periodic-table.json")
+f3 = open("./periodic-table.json")
 PERIODIC_TABLE = json.load(f3)
