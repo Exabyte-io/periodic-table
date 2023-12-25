43694cc50918b13b40e4a6265dd436f140065228
Timur Bazhirov <timur@exabyte.io>
2023-12-25 04:28:54 -0800

    chore: update PY implementation
    Nest under `mat3ra` subdirectory

--- src/py/mat3ra/periodic_table/__init__.py
+++ src/py/mat3ra/periodic_table/__init__.py
@@ -1,5 +1,8 @@
 import json
 
+# Note: the paths are relative to the root of the project.
+# This is meant to work with a non-editable (packaged/zipped) installation, e.g. `pip install ."[tests]"`
+
 f1 = open("./element-bonds.json")
 ELEMENT_BONDS = json.load(f1)
 f2 = open("./element-colors.json")
